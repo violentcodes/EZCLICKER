c0f92676834e80f2fbf4d5ce9fa7835c1c64c641
Saksham Sharma <sakshamsharma03221@gmail.com>
2024-06-14 17:03:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import mouse , pyfiglet , colorama
 from colorama import Fore
 a = pyfiglet.figlet_format("EZCLICKER") 
 print(Fore.MAGENTA + a)
-b = int(input("enter range:"))
+print(Fore.RED + "made by saksham sharma/violent codes")
+b = int(input(Fore.MAGENTA + "enter range:"))
 for i in range(b):
     mouse.click('left')
 
